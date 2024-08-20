a5f644beddd29865c9f7d102553994db5bb81214
Vadim <john.smithkarter2016@mail.ru>
2024-08-20 16:56:16 +0300

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mote/app.dart';
+import 'package:mote/core/common/presentation/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
